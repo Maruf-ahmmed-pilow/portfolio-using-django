b8b8423e122cb9085e944dc50360b0c44366aaa2
Pilow ahmmed <marufahmedpilow727@gmail.com>
2023-04-11 08:19:13 +0600

    second commit

---
+++ authapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def handlelogin(request):
+    return render(request, 'login.html')
+
+
+def handlelogout(request):
+    return render(request, 'logout.html')
