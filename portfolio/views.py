4dd8ca410dc0daedef16aeb6913fefc821ef95f6
maruf-ahmed <marufahmedpilow727@gmail.com>
2023-04-17 11:38:41 +0600

    modify code

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from portfolio.models import Contact
 
 # Create your views here.
 def home(request):
@@ -8,5 +10,24 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+
+def services(request):
+    return render(request, 'services.html')
+  
+
 def contact(request):
+    if request.method=="POST":
+       fname=request.POST.get('name')
+       femail=request.POST.get('email')
+       fphoneno=request.POST.get('num')
+       fdesc=request.POST.get('desc')
+       query=Contact(name=fname,email=femail,phonenumber=fphoneno,description=fdesc)
+       query.save()
+       messages.success(request,"Thanks for contacting us. We will get by you soon!")
+       
+       
+       return redirect('/contact')
+     
+
+
     return render(request, 'contact.html')
